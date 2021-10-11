568b3c684fcc88b90f234db9be941c8b40aba296
dibya20420 <dibya.ranjan031@gmail.com>
2021-10-11 20:23:16 +0530

    html , css and js file is added

---
+++ game.js
@@ -0,0 +1,14 @@
+class Game{
+    constructor(){
+        this.currentPlayer="X";
+        this.board = new Array(9).fill(null);
+    }
+    // box ClickedMethod
+    //some One is winning
+    // end game
+    // change the player
+    // restart method
+    boxClicked(){
+        
+    }
+}
