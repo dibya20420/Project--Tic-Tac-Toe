e7f059c0221c4cc89965bc5a03200a55094f4bd0
dibya20420 <dibya.ranjan031@gmail.com>
2021-10-11 21:11:30 +0530

    box clicked method is added

--- game.js
+++ game.js
@@ -8,7 +8,39 @@ class Game{
     // end game
     // change the player
     // restart method
-    boxClicked(){
+    boxClicked(e){
+        //console.log(e.target.id);
+        // check if that box is already filled
+        // if that place is empty put X|O
+        // update the ui
+        //check if the player wins
+        // check draw
+        //  change player
+        const id = e.target.id;
+        if(!this.board[id]){
+            this.board[id]= this.currentPlayer;
+           // console.log(this.board);
+           e.target.innerText = this.currentPlayer;
+           if(this.isWinning){
+               //end the game
+               // return
+           }
+           else if(!this.board.some((item)=> item===null)){
+               //end the game if it draw and this will only happen when the place will be filled
+               // return
+           }
+           this.changePlayer;
+        }
+    }
+    changePlayer(){
         
     }
 }
+// creating an instance so that this class give us a object
+const game = new Game();
+
+function makeMove(e){
+    game.boxClicked(e);
+}
+const boxes = document.querySelectorAll('.box');
+boxes.forEach((box)=> box.addEventListener('click',makeMove));
